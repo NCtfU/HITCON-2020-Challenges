73b605b76eeafb2c3de930721ee0c9b70285ebd4
davidhcefx <davidhu0903ex3@gmail.com>
2020-09-08 12:24:15 +0800

    Renaming

--- mysql/setup_db.sql
+++ mysql/setup_db.sql
@@ -13,9 +13,9 @@ FLUSH PRIVILEGES;
 -- DROP TABLE chat.users;
 CREATE TABLE chat.users (username CHAR(10), password CHAR(15), icon CHAR(15),
                          account_type INT, last_active INT);
-INSERT INTO chat.users VALUES ('BBBBB', 'nc7FU_W3LL',  'man01.png', 0, 1);
-INSERT INTO chat.users VALUES ('CCCCC', '????????',    'man02.png', 1, 1);
-INSERT INTO chat.users VALUES ('DDDDD', '???????',     'man04.png', 1, 2);
+INSERT INTO chat.users VALUES ('娜塔莎·羅曼諾夫', 'bl42kkk_31d0ww',  'man01.png', 0, 1);
+INSERT INTO chat.users VALUES ('里克·梅森', 'ri244_ma5000n',    'man02.png', 1, 1);
+INSERT INTO chat.users VALUES ('阿列克謝·肖斯塔科夫', 'R3D_6u4rd14n',     'man04.png', 1, 2);
 INSERT INTO chat.users VALUES ('Lily',  'wh0_15_111y', 'man03.png', 1, 2);
 INSERT INTO chat.users VALUES ('autis', 'cut3',        'default.png', 2, 0); -- 放進去誰嗎?
 INSERT INTO chat.users VALUES ('david', 'Hand50M3',    'default.png', 2, 0);
@@ -26,17 +26,17 @@ INSERT INTO chat.users VALUES ('yen',   '84D_9uy',     'default.png', 2, 0);
 CREATE TABLE chat.message (isMessage BOOL, deleted BOOL, name VARCHAR(15), time INT,
                            call_duration CHAR(11), msg TEXT);
 
-INSERT INTO chat.message VALUES (1, 0, 'BBBBB',   5, 0, '事情都到這地步了，該怎麼辦....');
-INSERT INTO chat.message VALUES (1, 0, 'CCCCC',   5, 0, '還是我們明天來討論一下，大家一起想想怎麼處理比較好？');
-INSERT INTO chat.message VALUES (1, 0, 'BBBBB',   5, 0, '好');
+INSERT INTO chat.message VALUES (1, 0, '娜塔莎·羅曼諾夫',   5, 0, '事情都到這地步了，該怎麼辦....');
+INSERT INTO chat.message VALUES (1, 0, '里克·梅森',   5, 0, '還是我們明天來討論一下，大家一起想想怎麼處理比較好？');
+INSERT INTO chat.message VALUES (1, 0, '娜塔莎·羅曼諾夫',   5, 0, '好');
 
-INSERT INTO chat.message VALUES (1, 0, 'BBBBB',   4, 0, '有聲音嗎？');
-INSERT INTO chat.message VALUES (1, 0, 'DDDDD',   4, 0, '有了');
+INSERT INTO chat.message VALUES (1, 0, '娜塔莎·羅曼諾夫',   4, 0, '有聲音嗎？');
+INSERT INTO chat.message VALUES (1, 0, '阿列克謝·肖斯塔科夫',   4, 0, '有了');
 INSERT INTO chat.message VALUES (0, 0, '',        4, 27, '');
-INSERT INTO chat.message VALUES (1, 0, 'BBBBB',   4, 0, '今天很感謝有你們的幫忙！！TAT');
+INSERT INTO chat.message VALUES (1, 0, '娜塔莎·羅曼諾夫',   4, 0, '今天很感謝有你們的幫忙！！TAT');
 INSERT INTO chat.message VALUES (1, 0, 'Lily',    4, 0, '(o^∀^)');
 
-INSERT INTO chat.message VALUES (1, 0, 'CCCCC',   2, 0, '話說我們要不換個平台啊，總覺得在這邊講話有被監聽的感覺 (還是我的錯覺 ^_^;) ');
+INSERT INTO chat.message VALUES (1, 0, '里克·梅森',   2, 0, '話說我們要不換個平台啊，總覺得在這邊講話有被監聽的感覺 (還是我的錯覺 ^_^;) ');
 INSERT INTO chat.message VALUES (1, 0, 'Lily',    2, 0, '同意+1');
-INSERT INTO chat.message VALUES (1, 1, 'BBBBB',   2, 0, '改用這個好了，密碼老樣子');
-INSERT INTO chat.message VALUES (1, 1, 'BBBBB',   2, 0, '? qqgnoe 的 url 咧');
+INSERT INTO chat.message VALUES (1, 1, '娜塔莎·羅曼諾夫',   2, 0, '改用這個好了，密碼老樣子');
+INSERT INTO chat.message VALUES (1, 1, '娜塔莎·羅曼諾夫',   2, 0, '? qqgnoe 的 url 咧');
